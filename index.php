190ad47a79d0fdda1eb9a1b92a4e96442882419c
Kishan Maurya <kkmaurya.0095@gmail.com>
2019-09-27 23:06:14 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Animated login form</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<form action="index.php" class="box" method="post">
+		<h1>Login</h1>
+		<input type="text" name="" placeholder="Enter the Name">
+		<input type="password" name="" placeholder="Password">
+		<input type="button" name="Register" value="Login">
+	</form>
+</body>
+</html>
